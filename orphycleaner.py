fc0aece353f85f086dce24fd3c5b82264dcc0183
Jozef Gaal <dodog@users.noreply.github.com>
2025-08-22 22:55:46 +0200

    Update orphycleaner.py

--- orphycleaner.py
+++ orphycleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 ##
 #     Project: OrphyCleaner GUI- Orphaned Config Folder Cleaner
 # Description: Scans your home directory for orphaned config folders
